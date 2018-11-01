d2d2a6c33e4d9ea66ac3530075f5a9b4e901ada8
maggie0408 <1210590780@qq.com>
2018-11-01 17:47:27 +0800

    add carousel to index.html and function to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,32 @@
 $(function(){
-
+    var num=0;
+    var timer=null;
+    go();
+    $("#box").mouseenter(function(){
+        clearInterval(timer);
+    }).mouseleave(function(){
+        go();
+    });
+    function go(){
+        timer=setInterval(function(){
+            num++;
+            if(num>4){
+                num=1;
+                $("#box>ul").css("margin-left","0");                     
+            }
+            if(num==4){
+                $("#box>ol>li").eq(0).addClass("current").siblings().removeClass("current");
+            }else{
+                $("#box>ol>li").eq(num).addClass("current").siblings().removeClass("current");           
+            }
+            $("#box>ul").animate({"marginLeft":-100*num+"%"},580);              
+        },3000);        
+    };
+    $("#box>ol>li").each(function(index){
+        $(this).click(function(){
+            num=index;
+            $(this).eq(num).addClass("current").siblings().removeClass("current");
+            $("#box>ul").stop().animate({"marginLeft":-100*num+"%"},580);
+        })
+    })
 });
