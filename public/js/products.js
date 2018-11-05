69942b4798bb5141c4393a6bb9b2bfca0abb9374
maggie0408 <1210590780@qq.com>
2018-11-05 19:01:28 +0800

    update products

--- public/js/products.js
+++ public/js/products.js
@@ -1,7 +1,41 @@
 $(function(){
+    var canMove=false;
+    var pl=$('.price').offset().left,pr=pl+$(".price").width();
     //价格范围选取
-    $("#minPrice").click(function(e){
-        var diffX = e.clientX - $("#minPrice").offsetX; 
-        console.log(e.offsetX);
+    minPrice.onmousedown=function(e){
+        canMove=true;  
+        var minOffsetX=e.offsetX; 
+        window.onmousemove=function(e){
+            if(canMove){               
+                var l1 = minOffsetX - $("#minPrice").offset().left; 
+                var leftLength = e.offsetX - pl - l1;
+                var diff=$("#maxPrice").offset().left-e.offsetX-15;
+                if(diff>0){$("#minPrice").css("left", leftLength + "px");}
+                
+                //$("#minPrice").children().i
+            }
+        }  
+    };
+    maxPrice.onmousedown=function(e){
+        canMove=true;   
+        var maxOffsetX=e.offsetX;     
+        window.onmousemove=function(e){
+            if(canMove){ 
+                var l2 = maxOffsetX - $("#maxPrice").offset().left;
+                var rightLength = $(".price").width()-(e.offsetX-pl-l2+15);
+                var diff=$("#maxPrice")
+                $("#maxPrice").css("right",rightLength+"px");
+            }
+        };
+    };
+    
+    minPrice.onmouseup=function(e){
+        canMove=false;
+    }
+    maxPrice.onmouseup=function(e){
+        canMove=false;
+    }
+    $(".price").mouseleave(function(){
+        canMove=false;
     })
 })
