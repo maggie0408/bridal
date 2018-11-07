310c24ad91dc340624a6cd990e7201b60e8e6c05
maggie0408 <1210590780@qq.com>
2018-11-07 18:09:33 +0800

    update priceRange of products files

--- public/js/products.js
+++ public/js/products.js
@@ -1,41 +1,117 @@
 $(function(){
-    var canMove=false;
-    var pl=$('.price').offset().left,pr=pl+$(".price").width();
-    //价格范围选取
-    minPrice.onmousedown=function(e){
-        canMove=true;  
-        var minOffsetX=e.offsetX; 
-        window.onmousemove=function(e){
-            if(canMove){               
-                var l1 = minOffsetX - $("#minPrice").offset().left; 
-                var leftLength = e.offsetX - pl - l1;
-                var diff=$("#maxPrice").offset().left-e.offsetX-15;
-                if(diff>0){$("#minPrice").css("left", leftLength + "px");}
-                
-                //$("#minPrice").children().i
-            }
-        }  
-    };
-    maxPrice.onmousedown=function(e){
-        canMove=true;   
-        var maxOffsetX=e.offsetX;     
-        window.onmousemove=function(e){
-            if(canMove){ 
-                var l2 = maxOffsetX - $("#maxPrice").offset().left;
-                var rightLength = $(".price").width()-(e.offsetX-pl-l2+15);
-                var diff=$("#maxPrice")
-                $("#maxPrice").css("right",rightLength+"px");
-            }
-        };
-    };
+    function $dom(id){
+        return document.getElementById(id);
+    }
+
+    function _on(target,eventName,handler){
+        target.addEventListener(eventName,handler,false);
+    }
+
+    // get elements
+    var $maxPrice=$dom('maxPrice');
+    var $minPrice=$dom('minPrice');
+    var $ctl=$dom('dragctl');
+    var $bar=$dom('dragbar');
     
-    minPrice.onmouseup=function(e){
-        canMove=false;
-    }
-    maxPrice.onmouseup=function(e){
-        canMove=false;
-    }
-    $(".price").mouseleave(function(){
-        canMove=false;
+    var minOffsetX,maxOffsetX,minClientX,maxClientX;
+    var canMove=false;
+    
+
+    // boundary of dragger bar size
+    var boundaryBar={
+        max:$ctl.getBoundingClientRect().width,
+        size:"",
+        min:0,
+        Fixed_left:$ctl.getBoundingClientRect().left,
+        Fixed_right:$ctl.getBoundingClientRect().right,
+        left:$bar.getBoundingClientRect().left,
+        right:$bar.getBoundingClientRect().right
+    };
+
+    var length={
+        left:"",
+        right:""
+    };
+
+    var size=15;
+
+    // price
+    var price={
+        max:function(){
+            return Math.floor(price.maxBar/boundaryBar.max*270)+30;
+        },
+        min:function(){
+            return Math.floor(price.minBar/boundaryBar.max*270)+30;
+        },
+        maxBar:boundaryBar.max,
+        minBar:boundaryBar.min
+    };
+
+
+    // event listener
+    _on($maxPrice,"drag",function(eve){
+        eve.stopPropagation();
+        if(canMove){
+            maxClientX = eve.clientX;
+            length.right = boundaryBar.Fixed_right+15;//boundaryBar.right - maxClientX + maxOffsetX - 15;
+
+            if (length.right > 0) {
+                price.maxBar = boundaryBar.max - length.right;
+                if (price.maxBar - price.minBar > 30) {
+                    $maxPrice.style.right = length.right + "px";
+                    $bar.style.right = length.right + "px";
+                    $bar.style.width=boundaryBar.max-length.right+"px";
+                }
+            }
+            $maxPrice.style.top="-4px";
+        }            
+    });
+
+    _on($minPrice,"drag",function(eve){
+        eve.stopPropagation();
+        if(canMove){            
+            minClientX = eve.clientX;
+            length.left = minClientX - boundaryBar.left - minOffsetX;
+
+            if (length.left > 0) {
+                price.minBar = length.left;
+
+                if (!length.right) length.right = 0;
+                price.maxBar = boundaryBar.max - length.right;
+
+                if (price.maxBar - price.minBar > 30) {
+                    $minPrice.style.left = length.left + "px";
+                    $bar.style.left = length.left + "px";
+                    $bar.style.width=boundaryBar.max-length.left+"px";
+                }
+            }
+            $minPrice.style.top="-4px";
+        }
+    });
+
+    _on($maxPrice,"dragstart",function(eve){
+        eve.stopPropagation();
+        maxOffsetX=eve.offsetX;
+        maxClientX=eve.clientX;
+        canMove=true;
+    })
+
+    _on($minPrice,"dragstart",function(eve){
+        eve.stopPropagation();
+        minOffsetX=eve.offsetX;
+        canMove=true;
+    })
+
+
+    _on($maxPrice,"dragend",function(eve){
+        maxClientX=eve.clientX;
+        length.right=boundaryBar.right-maxClientX+maxOffsetX-15;
+        eve.stopPropagation();
+    })
+
+    _on($minPrice,"dragend",function(eve){
+        minClientX=eve.clientX;
+        length.left=maxClientX-boundaryBar.left-minOffsetX;
+        eve.stopPropagation();
     })
 })
